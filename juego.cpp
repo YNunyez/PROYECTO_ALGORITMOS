79ff14e797aafa4949e045538ae6dece1dfae33b
YNunyez <161104993+YNunyez@users.noreply.github.com>
2024-04-13 00:37:51 -0600

    Update juego.cpp
    Se introdujo el test_entidad.h

--- juego.cpp
+++ juego.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <time.h>
 #include <locale.h>
-#include "test_listadoble.h"
+#include "test_entidad.h"
 using namespace std;
 
 
@@ -68,113 +68,34 @@ int *tirardados(int *y){
 	return y;
 }
 
-
-int * genposind(){
+/*int * genposind(){
 	int y[2];
 	int *u = y;
 	u[0]=1 + rand() % 10;
 	u[1]=1 + rand() % 10;
 	return u;
-}
-
-
-ListaDoble genposmul(int _n){
-	ListaDoble l = ListaDoble();
-	int i=1;
-	int a,b,*o;
-	while (i<=_n){
-		o = genposind();
-		/*o = int[2];
-		o[0]=1 + rand() % 10;
-		o[1]=1 + rand() % 10;*/
-		//l.imprimir();
-		l.insertarOrdenado(o);
-		//l.imprimir();
-		i++;
-	}
-	return l;
-}
-
-
-
-
-class entidad{
-	private:
-		string name;
-		int PV;
-		int PA;
-		ListaDoble pos;
-	public:
-		entidad(string,int,int,ListaDoble);
-		void mostrar();
-		string getname();
-		int getPV();
-		int getPA();
-		void setPos(int);
-		ListaDoble getPos();
-		void atacar();
-};
-
-entidad::entidad(string _name,int _PV, int _PA,ListaDoble _pos){
-	name=_name;
-	PV=_PV;
-	PA=_PA;
-	pos=_pos;	
-}
-
-void entidad::mostrar(){
-	cout<<"Nombre: "<<name<<endl<<"PV: "<<PV<<endl<<"PA: "<<PA<<endl<<"pos: ";
-	pos.imprimir();
-}
-
-string entidad::getname(){
-	return name;
-}
-	
-int entidad::getPV(){
-	return PV;
-}
-
-int entidad::getPA(){
-	return PA;
-}
-
-void entidad::setPos(int _n){
-	pos=genposmul(_n);
-}
-ListaDoble entidad::getPos(){
-	return pos;
-}
-
-
+}*/
 
 
 
 int main(){
-	
+	srand(time(NULL));
 	setlocale(LC_CTYPE, "Spanish");
+	mapa();
 	int pos[]={0,0};
 	int p,*o=pos;
 	ListaDoble posj1 = ListaDoble(),posen = ListaDoble();
 	entidad en = entidad("a",0,0,posen);
 	posj1.insertarOrdenado(o);
-	entidad j1 = entidad("Errësirë",2,3,posj1);
+	entidad j1 = entidad("Errësirë",3,3,posj1);
+	j1.mostrar();
+	j1.setPosP(tirardados(posj1.getPri()));
 	j1.mostrar();
 	string nen[10]={"Arañas","Golems de barro","Zorros oscuros","Duendes","Esqueletos","Orcos","Golems de piedra","Espectros","Demonios","Arcangel"};
-	en = entidad(nen[1],2,5,genposmul(15));
-	
-	
-	//string pos[2]={"A","0"};
-	int posr[2]={1,1};
-	srand(time(NULL));
+	en = entidad(nen[2],2,5,genpos(15));
+	en.mostrar();
 	string fil[10]={"1","2","3","4","5","6","7","8","9","10"};
 	string col[10]={"A","B","C","D","E","F","G","H","I","J"};
 	
-	/*int *o=posr;
-	//int *o={1,1};
-	cout<<"INICIO: "<<col[o[0]-1]<<fil[o[1]-1]<<endl<<endl;
-	o = tirardados(o);
-	cout<<"POSICION: "<<col[o[0]-1]<<fil[o[1]-1]<<endl<<endl;
-	getch();*/
 	return 0;
 }
