800b3b39d1da42409fb5cdcd3a244a94f8595742
YNunyez <161104993+YNunyez@users.noreply.github.com>
2024-04-19 18:27:17 -0600

    Update juego.cpp

--- juego.cpp
+++ juego.cpp
@@ -7,19 +7,45 @@
 using namespace std;
 
 
-void mapa(){
-	cout<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"10 |   |   |   | P |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"9  |   |   |   |   |   |   |   |   |   |   |"<<endl;
+void mapa(int* _pj){
+	cout<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"10 |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"9  |   |   |   |   |   |   |   |   |   |   |"<<endl;
 	cout<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"8  |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl;
 	cout<<"7  |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"6  |   |   |   |   |   |   |   |   |   |   |"<<endl;
 	cout<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"5  |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl;
 	cout<<"4  |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"3  |   |   |   |   |   |   |   |   |   |   |"<<endl;
 	cout<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"2  |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl;
-	cout<<"1  |   |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"     A   B   C   D   E   F   G   H   I   J  "<<endl;
+	cout<<"1  | P |   |   |   |   |   |   |   |   |   |"<<endl<<"   +---+---+---+---+---+---+---+---+---+---+"<<endl<<"     A   B   C   D   E   F   G   H   I   J  "<<endl;
 	system("pause");
 	return;
 }
 
+//Genera una posición aleatoria
+int *_genpos(){
+	int *o= new int;
+	o[0]=1 + rand() % 10;
+	o[1]=1 + rand() % 10;
+	return o;
+}
 
+//RECIBE UN ENTERO Y RETORNA UNA LISTA DOBLE CON ESA CANTIDAD DE PARES ORDENADOS GENERADOS ALEATORIAMENTE
+ListaDoble genpos(int _n,ListaDoble _l){
+	ListaDoble l = ListaDoble();
+	int i=1;
+	int *o;
+	while (i<=_n){
+		o[0]=1 + rand() % 10;
+		o[1]=1 + rand() % 10;
+		if (o[0]==1){
+			o[1]=2 + rand() % 9;
+		}
+		if(!l.buscar(o)&&!_l.buscar(o)){
+			l.insertarOrdenado(o);
+			o=new int;
+			i++;
+		}		
+	}
+	return l;
+}
 
 int *tirardados(int *y){
 	int x,a,b,u=0;
@@ -67,27 +93,72 @@ int *tirardados(int *y){
 	return y;
 }
 
+
+bool buscar(entidad j1,entidad otro){
+	int *pj=j1.getPos();
+}
+
+
 // en main se crean los objetos(enemigos y jugador)
 int main(){
-	srand(time(NULL));				// para randomizar la funcion rand() ubicada en tirardados y otras funciones del código
+	srand(time(NULL));						// para randomizar la funcion rand() ubicada en tirardados y otras funciones del código
 	setlocale(LC_CTYPE, "Spanish");			// permite más carácteres en la consola
 	int pos[]={1,1};	
 	int p,*o=pos;
-	ListaDoble posj1 = ListaDoble(),posen = ListaDoble();
-	entidad en = entidad("a",0,0,posen);							
-	posj1.insertarOrdenado(o);
-	entidad j1 = entidad("Errësirë",3,3,posj1);
+	entidad en = entidad("a",0,0,o);							
+	entidad j1 = entidad("Errësirë",3,3,o);
+	ListaDoble ens = ListaDoble();
+	ListaDoble cofres = ListaDoble();
 	string nen[10]={"Arañas","Golems de barro","Zorros oscuros","Duendes","Esqueletos","Orcos","Golems de piedra","Espectros","Demonios","Arcangel"};
-	
+	string aliados[10]={"Arañas","Golems de barro","Zorros oscuros","Duendes","Esqueletos","Orcos","Golems de piedra","Espectros","Demonios","Arcangel"};
 	for(int piso=1;piso<11;piso++){
+		mapa(o);
+		o[0]=1;o[1]=1;
 		cout<<"Piso: "<<piso<<endl;
 		en.set_Name_PA_PV(nen[piso-1],piso,piso+1);
-		cout<<"Enemigos: ";
-		en.mostrar();
-		cout<<"Jugador: ";
-		j1.mostrar();
-		j1.setPosP(tirardados(posj1.getPri()));	
-		j1.mostrar();
+		ens = genpos(15,ens);
+		cofres = genpos(15,ens);
+		int *y=_genpos();
+		while(ens.buscar(y)!=NULL){
+			y=_genpos();
+		}
+		cout<<"___________"<<endl;
+		cout<<endl<<"Enemigos: "<<endl;
+		en.mostrar(0);
+		cout<<"Posiciones: ";
+		ens.imprimir();
+		cout<<endl<<"___________"<<endl<<"___________"<<endl;
+		cout<<endl<<"Cofres: ";
+		cofres.imprimir();
+		cout<<endl<<"___________"<<endl;
+		cout<<"___________"<<endl;
+		cout<<endl<<"Jugador: "<<endl;
+		j1.mostrar(2);
+		cout<<endl<<"___________"<<endl;
+		while(o[0]+o[1]<20){
+			j1.setPos(tirardados(j1.getPos()));	
+			if(ens.buscar(o)!=NULL){
+				cout<<endl<<"___________"<<endl<<endl;
+				en.mostrar(0);
+				cout<<endl<<"___________"<<endl<<endl;
+				j1.mostrar(2);
+				if(j1.enfrentar(en)){
+					ens.eliminar(o);
+					cout<<endl<<"___________"<<endl<<endl<<"Victoria"<<endl<<"___________"<<endl;
+				}
+				else{
+					cout<<endl<<"___________"<<endl<<endl<<"Derrota"<<endl<<"___________"<<endl;
+					return 0;
+				}
+				j1.mostrar(2);
+				cout<<endl;
+				ens.imprimir();
+			}
+			cout<<"___________"<<endl<<endl;
+			cout<<"Posición actual: ";j1.mostrar(1);
+			cout<<endl<<"___________"<<endl;
+			
+		}
 	}	
 	return 0;
 }
